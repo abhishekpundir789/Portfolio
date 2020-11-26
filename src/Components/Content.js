187bbabc8f816f2d967badd66fcff4d48343980f
abhishekpundir789 <apundir@algomau.ca>
2020-11-26 14:33:25 -0800

    update commit with creating components and styling css

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -7,9 +7,8 @@ function Content() {
     return (
         <div className = "Content-container">
             <img src = "images/image3.jpg" alt = "vancouver"></img>
-            <h1 className="display-4">Hi There,</h1><br></br>
-            <h3>My Name is Abhishek Pundir.</h3><br></br>
-            <h3>Learning Full Stack Web Development</h3><br></br>
+            <h1 className="display-4">Hi There, I'm Abhishek Pundir.</h1><br></br>
+            <h3 className="display">Learning Full Stack Web Development</h3><br></br>
             <div className = "Content-btns">
                 <Button className = "btns" buttonStyle = 'btn--outline' buttonSize = 'btn--large'>About Me </Button>
                 <Button className = "btns" buttonStyle = 'btn--primary' buttonSize = 'btn--large'>Portfolio </Button>
--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../../App.css';
+import Cards from '../Cards';
 import Content from '../Content';
+
 
 function Home(){
     return(
         <>
         <Content/>
+        <Cards />
         </>
     );
 }
