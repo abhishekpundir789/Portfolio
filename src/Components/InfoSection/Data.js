0117bd0943d5ce05e755d242bdc506df70abf88e
abhishekpundir789 <apundir@algomau.ca>
2020-12-16 20:25:21 -0800

    update commit with adding about me section

---
+++ src/Components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lihtText: true,
+    lightTextDesc: true,
+    topLine: 'Introduction',
+    headline: 'I am learning Full Stack Web Development from BCIT, Vancouver, BC, Canada.',
+    description: 'I have did Bachelors in Sceince(C.S) from India and Graduate Certificate in IT from Algoma University, Brampton, ON, Canada. Through the current course I will explore the IT field and will help me to land the dream job. Also through this course I will be having real time experience on various projects in different languages.',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/profile-pic.jpg'),
+    alt: 'Profile',
+    dark: true,
+    primary: true,
+    darkText: false
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import Sidebar from '../Components/Sidebar';
 import Navbar from '../Components/Navbar';
 import HeroSection from '../Components/HeroSection';
+import InfoSection from '../Components/InfoSection';
 
 const Home = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -15,6 +16,7 @@ const Home = () => {
          <Sidebar isOpen={isOpen} toggle = {toggle}/>
          <Navbar toggle= {toggle}/>   
          <HeroSection />
+         <InfoSection />
         </>
     )
 }
