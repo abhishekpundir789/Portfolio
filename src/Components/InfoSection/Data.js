6019a6bf32b7e45aabff5f3d2d795b46eca51682
abhishekpundir789 <apundir@algomau.ca>
2020-12-17 16:57:20 -0800

    update commit with removing button class

--- src/Components/InfoSection/Data.js
+++ src/Components/InfoSection/Data.js
@@ -1,33 +1,27 @@
 export const homeObjOne = {
     id: 'about',
     lightBg: false,
-    lihtText: false,
+    lihtText: true,
     lightTextDesc: true,
     topLine: 'Introduction',
     headline: 'I am learning System Software Development from BCIT, Vancouver, BC, Canada.',
     description: 'I did Bachelors in Sceince(C.S) from India and Graduate Certificate in IT from Algoma University, Brampton, ON, Canada. Also through this course I will be having real time experience on various projects in different languages.',
-    buttonLabel: 'Get Started',
     imgStart: false,
     img: require('../../images/svg-1.svg'),
     alt: 'Profile',
-    dark: true,
-    primary: true,
     darkText: false
 };
 
 export const homeObjTwo = {
     id: 'portfolio',
     lightBg: false,
-    lihtText: false,
-    lightTextDesc: true,
+    lihtText: true,
+    lightTextDesc: false,
     topLine: 'Projects',
     headline: 'Here are some list of projects:',
     description: '1) BC Tourism Website with only HTML and CSS. 2) Food Order Website Using JavaScript. 3) eCommerce Website using HTML, CSS and JavaScript. 4) MovieDB App using React. 5) eCommerce Website using Asp-Net C# ',
-    buttonLabel: 'Get Started',
-    imgStart: false,
+    imgStart: true,
     img: require('../../images/svg-1.svg'),
     alt: 'Profile',
-    dark: true,
-    primary: true,
     darkText: false
 };
