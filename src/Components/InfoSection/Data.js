5e930903c33c89090c0e6e65a623fb08eaca45c8
abhishekpundir789 <apundir@algomau.ca>
2020-12-17 21:27:38 -0800

    update commit

--- src/Components/InfoSection/Data.js
+++ src/Components/InfoSection/Data.js
@@ -7,7 +7,7 @@ export const homeObjOne = {
     headline: 'I am learning System Software Development from BCIT, Vancouver, BC, Canada.',
     description: 'I did Bachelors in Sceince(C.S) from India and Graduate Certificate in IT from Algoma University, Brampton, ON, Canada. Also through this course I will be having real time experience on various projects in different languages.',
     imgStart: false,
-    img: require('../../images/img-1.jpg'),
+    img: require('../../images/svg-1.svg'),
     alt: 'Profile',
     darkText: false
 };
