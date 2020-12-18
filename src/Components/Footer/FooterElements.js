51192c7a0d9ac91191bfbe4384fc3d7a49a4a04d
abhishekpundir789 <apundir@algomau.ca>
2020-12-17 20:27:30 -0800

    update commit with the footer section and social media icons

--- src/Components/Footer/FooterElements.js
+++ src/Components/Footer/FooterElements.js
@@ -15,53 +15,48 @@ maxx-width: 1100px;
 margin: 0 auto;
 `;
 
-export const FooterLinkContainer = styled.div`
-display: flex;
-justify-content: center;
-
-@media screen and (max-width: 820px){
-    padding-top: 32px;
-}
+export const SocialMedia = styled.section`
+max-width: 1000px;
+width: 100%;
 `;
 
-export const FooterLinksWrapper = styled.div`
+export const SocialMediaWrap = styled.div`
 display: flex;
+justify-content: space-between;
+align-items: center;
+max-width: 1100px;
+margin: 40px auto 0 auto;
 
 @media screen and (max-width: 820px){
     flex-direction: column;
 }
 `;
 
-export const FooterLinkItems = styled.div`
-display: flex;
-flex-direction: column;
-align-items: flex-start;
-margin: 16px;
-text-align: left;
-width: 160px;
-box-sizing: border-box;
+export const SocialLogo = styled(Link)`
 color: #fff;
-
-@media screen and (max-width: 420px){
-    margin: 0;
-    padding: 10px;
-    width: 100%;
-}
+justify: self: start;
+cursor: pointer;
+text-decoration: none;
+font-size: 1.5rem;
+display: flex;
+align-items: center;
+margin-bottom: 16px;
+font-weight: bold;
 `;
 
-export const FooterLinkTitle = styled.h1`
-font-size: 14px;
+export const WebsiteRights = styled.small`
+color: #fff;
 margin-bottom: 16px;
 `;
 
-export const FooterLink = styled(Link)`
-color: #fff;
-text-decoration: none;
-margin-bottom: 0.5rem;
-font-size: 14px;
+export const SocialIcons = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+width: 240px;
+`;
 
-&:hover{
-    color: #01bf71;
-    transition: 0.3s ease-in-out;
-}
+export const SocialIconLink = styled.a`
+color: #fff;
+font-size: 24px;
 `;
--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,19 +1,28 @@
 import React from 'react'
-import {FooterContainer, FooterWrap, FooterLinkContainer, FooterLinksWrapper, FooterLinkItems, FooterLinkTitle, FooterLink} from './FooterElements'
+import { FaFacebook, FaGithub, FaInstagram } from 'react-icons/fa'
+import {FooterContainer, FooterWrap, SocialMedia, SocialMediaWrap, SocialLogo, WebsiteRights, SocialIcons, SocialIconLink} from './FooterElements'
 
 const Footer = () => {
     return (
         <FooterContainer>
             <FooterWrap>
-                <FooterLinkContainer>
-                    <FooterLinksWrapper>
-                        <FooterLinkItems>
-                            <FooterLinkTitle>
-                                <FooterLink to="/github">About Us</FooterLink>
-                            </FooterLinkTitle>
-                        </FooterLinkItems>
-                    </FooterLinksWrapper>
-                </FooterLinkContainer>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to='/'>Welcome</SocialLogo>
+                        <WebsiteRights>Portfolio © {new Date().getFullYear()}All rights reserved</WebsiteRights>
+                        <SocialIcons>
+                            <SocialIconLink href="//https://www.instagram.com/abhishekpundir789/" target="https://www.instagram.com/abhishekpundir789/" aria-label="Instagram">
+                                <FaInstagram></FaInstagram>
+                            </SocialIconLink>
+                            <SocialIconLink href="//https://www.facebook.com/abhishek.pundir.14268" target="https://www.facebook.com/abhishek.pundir.14268" aria-label="Facebook">
+                                <FaFacebook></FaFacebook>
+                            </SocialIconLink>
+                            <SocialIconLink href="//https://github.com/abhishekpundir789" target="https://github.com/abhishekpundir789" aria-label="GitHub">
+                                <FaGithub></FaGithub>
+                            </SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
             </FooterWrap>
         </FooterContainer>
     )
